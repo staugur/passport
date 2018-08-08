58349c25210d6038dce49f083d7a6140bc900c3d
Mr.tao <staugur@vip.qq.com>
2018-08-08 17:46:03 +0800

    update README.md

--- README.md
+++ README.md
@@ -16,32 +16,51 @@ MIT
 ## Usage
 
 ```
-1. Requirement:
+1. 依赖:
     1.0 yum install -y gcc gcc-c++ python-devel libffi-devel openssl-devel mysql-devel
     1.1 git clone https://github.com/staugur/passport
     1.2 pip install -r requirements.txt
     1.3 MySQL需要导入misc/passport.sql数据库文件
 
-2. modify config.py or add environment variables(os.getenv key in the reference configuration item):
+2. 修改src/config.py中配置项, getenv函数后是环境变量及其默认值。
     2.1 修改GLOBAL全局配置项(主要是端口、日志级别)
     2.2 修改MODULES核心配置项账号认证模块的MYSQL信息
     2.3 修改PLUGINS插件配置项(主要是第三方登录)
 
-3. run:
+3. 运行:
     3.1 python main.py        #开发模式
     3.2 sh online_gunicorn.sh #生产模式
 ```
 
 
+## Misc
+
+1. cli
+ 
+```
+usage: cli.py [-h] [--refresh_loginlog] [--refresh_clicklog]
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --refresh_loginlog  刷入登录日志
+  --refresh_clicklog  刷入访问日志
+```
+
+
 ## TODO
-```
+
 0. redis sid存登录时设备信息
-1. 绑定邮箱手机、手机登录
+
+~~1. 绑定邮箱手机、手机登录~~
+
 2. 签到
+
 3. 用户行为记录
+
 4. 系统管理
+
 5. 安全
-```
+
 
 ## Design
 ![Design][1]
--- misc/passport.sql
+++ misc/passport.sql
@@ -1,16 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50720
-Source Host           : localhost:3306
-Source Database       : saintic
-
-Target Server Type    : MYSQL
-Target Server Version : 50720
-File Encoding         : 65001
-
-Date: 2018-03-19 15:55:53
+Date: 2018-08-08 17:39:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,7 +21,7 @@ CREATE TABLE `sso_apps` (
   `mtime` int(10) DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user_auth
@@ -45,15 +36,15 @@ CREATE TABLE `user_auth` (
   `verified` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否已验证 0-未验证 1-已验证',
   `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '用户状态 0-禁用 1-启用',
   `ctime` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '绑定时间',
-  `mtime` int(11) unsigned DEFAULT NULL COMMENT '更新绑定时间',
-  `etime` int(11) unsigned DEFAULT NULL COMMENT '到期时间，特指OAuth2登录',
-  `refresh_token` varchar(255) DEFAULT NULL COMMENT '第三方登录刷新token',
+  `mtime` int(11) unsigned DEFAULT '0' COMMENT '更新绑定时间',
+  `etime` int(11) unsigned DEFAULT '0' COMMENT '到期时间，特指OAuth2登录',
+  `refresh_token` varchar(255) DEFAULT '' COMMENT '第三方登录刷新token',
   PRIMARY KEY (`id`),
   UNIQUE KEY `identifier` (`identifier`) USING BTREE,
   KEY `status` (`status`) USING BTREE,
   KEY `idx_uid` (`uid`) USING BTREE,
   KEY `identity_type` (`identity_type`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COMMENT='用户授权表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户授权表';
 
 -- ----------------------------
 -- Table structure for user_loginlog
@@ -73,30 +64,31 @@ CREATE TABLE `user_loginlog` (
   `browser_family` varchar(30) CHARACTER SET utf8 DEFAULT NULL COMMENT '浏览器种类及版本，如chrome 60.0.3122',
   PRIMARY KEY (`id`),
   KEY `idx_uid_type_time` (`uid`,`login_type`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COMMENT='登录日志表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='登录日志表';
 
 -- ----------------------------
 -- Table structure for user_profile
 -- ----------------------------
 DROP TABLE IF EXISTS `user_profile`;
 CREATE TABLE `user_profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `uid` char(22) NOT NULL COMMENT '用户id',
   `register_source` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '注册来源 同identity_type 不可更改',
-  `register_ip` varchar(15) NOT NULL COMMENT '注册IP地址，不可更改',
-  `nick_name` varchar(49) DEFAULT NULL COMMENT '用户昵称',
+  `register_ip` varchar(15) NOT NULL DEFAULT '' COMMENT '注册IP地址，不可更改',
+  `nick_name` varchar(49) DEFAULT '' COMMENT '用户昵称',
   `domain_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '个性域名',
   `gender` tinyint(1) unsigned DEFAULT '2' COMMENT '用户性别 0-女 1-男 2-保密',
-  `birthday` int(10) unsigned DEFAULT NULL COMMENT '用户生日时间戳',
-  `signature` varchar(140) DEFAULT NULL COMMENT '用户个人签名',
+  `birthday` int(10) unsigned DEFAULT '0' COMMENT '用户生日时间戳',
+  `signature` varchar(140) DEFAULT '' COMMENT '用户个人签名',
   `avatar` varchar(255) DEFAULT '/static/images/avatar/default.png' COMMENT '头像',
-  `location` varchar(50) DEFAULT NULL COMMENT '地址',
-  `ctime` int(10) unsigned NOT NULL COMMENT '账号创建时间',
-  `mtime` int(10) unsigned DEFAULT NULL COMMENT '资料修改时间',
+  `location` varchar(50) DEFAULT '' COMMENT '地址',
+  `ctime` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '账号创建时间',
+  `mtime` int(10) unsigned DEFAULT '0' COMMENT '资料修改时间',
   `is_realname` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否实名认证 0-未实名 1-已实名',
   `is_admin` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否管理员 0-否 1-是',
-  `lock_nick_name` int(10) DEFAULT '-1' COMMENT '昵称锁，有效时间戳内表示加锁，-1表示无锁',
-  `lock_domain_name` tinyint(1) DEFAULT '-1' COMMENT '域名锁，0表示加锁 -1表示无锁',
-  PRIMARY KEY (`uid`),
+  `lock_nick_name` int(10) DEFAULT '1' COMMENT '昵称锁，10位有效时间戳内表示加锁，1表示无锁',
+  `lock_domain_name` tinyint(1) DEFAULT '1' COMMENT '域名锁，0表示加锁 1表示无锁',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `uid` (`uid`),
   UNIQUE KEY `dn` (`domain_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户个人资料表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='用户个人资料表';
